2bc982769bd9eefaa02cc343abdab0d02168d3f3
alekseisenkov <joschajoscha9@gmail.com>
2026-03-28 10:02:39 +0100

    Chess update

--- chess_client.py
+++ chess_client.py
@@ -1,8 +1,9 @@
-import copy
 import requests
 import pygame
 import json
+import pygame_gui
 
+api = "http://127.0.0.1:5000"
 piece_w1 = pygame.image.load("Chess_plt45.svg.png")
 piece_w2 = pygame.image.load("Chess_nlt45.svg.png")
 piece_w3 = pygame.image.load("Chess_blt45.svg.png")
@@ -17,36 +18,47 @@ piece_b4 = pygame.image.load("Chess_rdt45.svg.png")
 piece_b5 = pygame.image.load("Chess_qdt45.svg.png")
 piece_b6 = pygame.image.load("Chess_kdt45.svg.png")
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
-screen1 = pygame.Surface((800, 800), pygame.SRCALPHA)
+W,H = 800,835
+screen = pygame.display.set_mode((W, H))
+screen1 = pygame.Surface((W, H), pygame.SRCALPHA)
+font = pygame.font.Font(None, 50)
+
+
+WHITE_PAWN = 1
+BLACK_PAWN = -1
+
+images = {
+    WHITE_PAWN: piece_w1,
+    BLACK_PAWN: piece_b1
+}
+
+class Button:
+    def __init__(self, x, y, w, h, color,text):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.text = font.render(text, True, (0, 0, 0))
+        self.text_rect = self.text.get_rect(center=(self.x+w/2, self.y+h/2))
+    def is_clicked(self, x, y):
+        if self.x < x < self.x + self.w and self.y < y < self.y + self.h:
+            return True
+        else:
+            return False
+    def draw(self):
+        pygame.draw.rect(screen, (0,0,0), (self.x, self.y, self.w, self.h))
+        pygame.draw.rect(screen, self.color, (self.x+1, self.y+1, self.w-2, self.h-2))
+        screen.blit(self.text, self.text_rect)
+
 
 
 class ChessField:
     def __init__(self):
         self.board = [[0] * 8 for _ in range(8)]
-        for i in range(8):
-            self.board[i][1] = -1
-            self.board[i][6] = 1
-        self.board[0][0] = -4
-        self.board[7][0] = -4
-        self.board[0][7] = 4
-        self.board[7][7] = 4
-        self.board[1][0] = -2
-        self.board[1][7] = 2
-        self.board[6][0] = -2
-        self.board[6][7] = 2
-        self.board[2][0] = -3
-        self.board[2][7] = 3
-        self.board[5][0] = -3
-        self.board[5][7] = 3
-        self.board[4][0] = -5
-        self.board[4][7] = 5
-        self.board[3][0] = -6
-        self.board[3][7] = 6
-        self.boards = [copy.deepcopy(self.board)]
 
     def draw(self):
-        self.board = json.loads(requests.get('http://127.0.0.1:5000/show').text)
+        self.board = json.loads(requests.get(f'{api}/show/{id}').text)
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
                 pygame.draw.rect(screen, (212 + 33 * ((i + j) % 2), 174 + 48 * ((i + j) % 2), 125 + 54 * ((i + j) % 2)),
@@ -78,45 +90,88 @@ class ChessField:
                 if self.board[i][j] == -6:
                     screen.blit(piece_b6, (i * 100 + 5, j * 100 + 5))
         if piece_selected is not None:
-            ls = (requests.get(f"http://127.0.0.1:5000/search/{piece_selected[0]}/{piece_selected[1]}").text).split(" ")
-            if ls ==[""]:
+            ls = (requests.get(f"{api}/search/{id}/{piece_selected[0]}/{piece_selected[1]}").text).split(" ")
+            if ls == [""]:
                 return None
             ls = list(map(int, ls))
             for i in ls:
                 pygame.draw.circle(screen1, (135, 206, 235, 170), ((i % 8) * 100 + 50, (i // 8) * 100 + 50), 20)
 
     def move(self, from_x, from_y, to_x, to_y):
-        if requests.get(f"http://127.0.0.1:5000/move/{from_x}/{from_y}/{to_x}/{to_y}/true").text == "ok":
+        if requests.get(f"{api}/move/{id}/{from_x}/{from_y}/{to_x}/{to_y}/true").text == "ok":
             self.board[to_x][to_y] = self.board[from_x][from_y]
             self.board[from_x][from_y] = 0
-            self.boards.append(copy.deepcopy(self.board))
 
     def back_move(self):
-        if len(self.boards) > 1:
-            self.board = copy.deepcopy(self.boards[-2])
-            self.boards.pop()
-            requests.get("http://127.0.0.1:5000/reverse")
+        requests.get(f"{api}/reverse/{id}")
 
 
+manager = pygame_gui.UIManager((800, 800))
+input_box = pygame_gui.elements.UITextEntryLine(
+    relative_rect=pygame.Rect((250, 505), (300, 50)),
+    manager=manager
+)
+
+
+clock = pygame.time.Clock()
 chess_field = ChessField()
 piece_selected = None
+in_game = False
+create_button = Button(250, 325, 300, 75, (255, 255, 255), "create a game")
+login_button = Button(250, 425, 300, 75, (255, 255, 255), "log in a game")
+norm_chess_button = Button(140, 625, 250, 75, (125, 125, 125), "chess")
+not_norm_chess_button = Button(410, 625, 250, 75, (255, 255, 255), "drunck chess")
+id = None
+norm_chess = "true"
 while True:
+    time_delta = clock.tick(60) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            if piece_selected is None:
-                piece_selected = x // 100, y // 100
+            if in_game:
+                if piece_selected is None:
+                    piece_selected = x // 100, y // 100
+                else:
+                    chess_field.move(piece_selected[0], piece_selected[1], x // 100, y // 100)
+                    piece_selected = None
             else:
-                chess_field.move(piece_selected[0], piece_selected[1], x // 100, y // 100)
-                piece_selected = None
+                if create_button.is_clicked(x, y):
+                    in_game = True
+                    id = int(requests.get(f"{api}/crate_game/{norm_chess}").text)
+                    text = font.render(str(id), True, (0, 0, 0))
+                    text_rect = text.get_rect(center=(W/2, H-35/2))
+                if login_button.is_clicked(x, y):
+                    in_game = True
+                    id = int(input_box.get_text())
+                    text = font.render(str(id), True, (0, 0, 0))
+                    text_rect = text.get_rect(center=(W/2, H-35/2))
+                if norm_chess_button.is_clicked(x, y):
+                    norm_chess = "true"
+                    norm_chess_button.color = (125, 125, 125)
+                    not_norm_chess_button.color = (255, 255, 255)
+                if not_norm_chess_button.is_clicked(x, y):
+                    norm_chess = "false"
+                    norm_chess_button.color = (255, 255, 255)
+                    not_norm_chess_button.color = (125, 125, 125)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
+            if event.key == pygame.K_BACKSPACE and in_game:
                 chess_field.back_move()
-    screen.fill((255, 255, 255))
-    screen1.fill((255, 255, 255, 0))
-    chess_field.draw()
-    screen.blit(screen1, (0, 0))
+        manager.process_events(event)
+    if in_game:
+        screen.fill((255, 255, 255))
+        screen1.fill((255, 255, 255, 0))
+        chess_field.draw()
+        screen.blit(screen1, (0, 0))
+        screen.blit(text, text_rect)
+    else:
+        screen.fill((255, 255, 255))
+        create_button.draw()
+        login_button.draw()
+        norm_chess_button.draw()
+        not_norm_chess_button.draw()
+        manager.update(time_delta)
+        manager.draw_ui(screen)
     pygame.display.update()
     pygame.time.wait(100)
--- main.py
+++ main.py
@@ -1,144 +1,227 @@
 import random
 from flask import Flask, request
-import copy
 
 app = Flask(__name__)
 
-Field = [[0] * 8 for _ in range(8)]
-for i in range(8):
-    Field[i][1] = -1
-    Field[i][6] = 1
-Field[0][0] = -4
-Field[7][0] = -4
-Field[0][7] = 4
-Field[7][7] = 4
-Field[1][0] = -2
-Field[1][7] = 2
-Field[6][0] = -2
-Field[6][7] = 2
-Field[2][0] = -3
-Field[2][7] = 3
-Field[5][0] = -3
-Field[5][7] = 3
-Field[4][0] = -5
-Field[4][7] = 5
-Field[3][0] = -6
-Field[3][7] = 6
 
-Fields = [copy.deepcopy(Field)]
+class Game():
+    def __init__(self,normal_chess):
+        self.Field = [[0] * 8 for _ in range(8)]
+        for i in range(8):
+            self.Field[i][1] = -1
+            self.Field[i][6] = 1
+        self.Field[0][0] = -4
+        self.Field[7][0] = -4
+        self.Field[0][7] = 4
+        self.Field[7][7] = 4
+        self.Field[1][0] = -2
+        self.Field[1][7] = 2
+        self.Field[6][0] = -2
+        self.Field[6][7] = 2
+        self.Field[2][0] = -3
+        self.Field[2][7] = 3
+        self.Field[5][0] = -3
+        self.Field[5][7] = 3
+        self.Field[4][0] = -5
+        self.Field[4][7] = 5
+        self.Field[3][0] = -6
+        self.Field[3][7] = 6
 
-@app.route('/move/<from_x>/<from_y>/<to_x>/<to_y>/<move>')
-def move(from_x, from_y, to_x, to_y, move):
-    s = request.args.get('select')
-    from_x, from_y, to_x, to_y = int(from_x), int(from_y), int(to_x), int(to_y)
-    if Field[from_x][from_y] * Field[to_x][to_y] > 0:
-        return "ne_ok"
-    if Field[from_x][from_y] in (1, -1):
-        if Field[to_x][to_y] == 0 and to_y == from_y - 1 * Field[from_x][from_y] and to_x == from_x:
-            if move == "true":
-                Field[to_x][to_y] = Field[from_x][from_y]
+        self.normal_chess = normal_chess
+        self.Fields = []
+        self.move = True
+
+        self.move_funcs = {
+            0: lambda _, __, ___, ____, _____: None,
+            1: self.move_pawn,
+            -1: self.move_pawn,
+            2: self.move_knight,
+            -2: self.move_knight,
+            3: self.move_bishop,
+            -3: self.move_bishop,
+            4: self.move_rook,
+            -4: self.move_rook,
+            5: self.move_queen,
+            -5: self.move_queen,
+            6: self.move_king,
+            -6: self.move_king,
+        }
+
+    def move_pawn(self, from_x, from_y, to_x, to_y, do_move):
+        if self.Field[to_x][to_y] == 0 and to_y == from_y - 1 * self.Field[from_x][from_y] and to_x == from_x:
+            if do_move == "true":
+                self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                self.Field[to_x][to_y] = self.Field[from_x][from_y]
                 if to_y == 0:
-                    Field[to_x][to_y] = random.randint(2,6)
-                    print("yay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\n")
+                    self.Field[to_x][to_y] = random.randint(2, 6)
                 if to_y == 7:
-                    Field[to_x][to_y] = random.randint(-6,-2)
-                    print("yay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\nyay\n")
-                Field[from_x][from_y] = 0
-                Fields.append(copy.deepcopy(Field))
+                    self.Field[to_x][to_y] = random.randint(-6, -2)
+                self.Field[from_x][from_y] = 0
             return "ok"
-        if Field[from_y - 1 * Field[from_x][from_y]][from_x] == 0 and from_y - 2 * Field[from_x][from_y] == to_y and from_y in (1, 6) and to_x == from_x:
-            if move == "true":
-                Field[to_x][to_y] = Field[from_x][from_y]
-                Field[from_x][from_y] = 0
-                Fields.append(copy.deepcopy(Field))
+        if self.Field[from_x][from_y - 1 * self.Field[from_x][from_y]] == 0 and from_y - 2 * self.Field[from_x][
+            from_y] == to_y and from_y in (1, 6) and to_x == from_x:
+            if do_move == "true":
+                self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                self.Field[from_x][from_y] = 0
             return "ok"
-        if Field[to_x][to_y] != 0 and to_y == from_y - 1 * Field[from_x][from_y] and (
+        if self.Field[to_x][to_y] != 0 and to_y == from_y - 1 * self.Field[from_x][from_y] and (
                 to_x == from_x + 1 or to_x == from_x - 1):
-            if move == "true":
-                Field[to_x][to_y] = Field[from_x][from_y]
-                Field[from_x][from_y] = 0
-                Fields.append(copy.deepcopy(Field))
+            if do_move == "true":
+                self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                if to_y == 0:
+                    self.Field[to_x][to_y] = random.randint(2, 6)
+                if to_y == 7:
+                    self.Field[to_x][to_y] = random.randint(-6, -2)
+                self.Field[from_x][from_y] = 0
             return "ok"
-    if Field[from_x][from_y] in (2, -2):
+        return "ne_ok"
+
+    def move_bishop(self, from_x, from_y, to_x, to_y, do_move):
+        if (abs(to_y - from_y) == abs(to_x - from_x) and
+                [self.Field[from_x + i * ((to_x - from_x) // abs(from_x - to_x))][
+                     from_y + i * ((to_y - from_y) // abs(from_y - to_y))]
+                 for i in range(1, abs(from_x - to_x))] == [0] * (abs(from_x - to_x) - 1)):
+            if do_move == "true":
+                self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                self.Field[from_x][from_y] = 0
+            return "ok"
+        return "ne_ok"
+
+    def move_queen(self, from_x, from_y, to_x, to_y, do_move):
+        if self.move_bishop(from_x, from_y, to_x, to_y, do_move) == "ok" or self.move_rook(from_x, from_y, to_x, to_y,
+                                                                                        do_move) == "ok":
+            return "ok"
+
+    def move_king(self, from_x, from_y, to_x, to_y, do_move):
+        if abs(to_y - from_y) <= 1 and abs(to_x - from_x) <= 1:
+            if do_move == "true":
+                self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                self.Field[from_x][from_y] = 0
+            return "ok"
+        return "ne_ok"
+
+    def move_rook(self, from_x, from_y, to_x, to_y, do_move):
+        if to_x == from_x:
+            if [self.Field[from_x][i] for i in range(min(from_y, to_y) + 1, max(from_y, to_y))] == [0] * (
+                    abs(from_y - to_y) - 1):
+                if do_move == "true":
+                    self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                    self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                    self.Field[from_x][from_y] = 0
+                return "ok"
+        elif to_y == from_y:
+            if [self.Field[i][from_y] for i in range(min(from_x, to_x) + 1, max(from_x, to_x))] == [0] * (
+                    abs(from_x - to_x) - 1):
+                if do_move == "true":
+                    self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                    self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                    self.Field[from_x][from_y] = 0
+                return "ok"
+        return "ne_ok"
+
+    def move_knight(self, from_x, from_y, to_x, to_y, do_move):
         if ((abs(to_y - from_y) == 1 and abs(to_x - from_x) == 2) or (
                 abs(to_y - from_y) == 2 and abs(to_x - from_x) == 1)):
-            if move == "true":
-                Field[to_x][to_y] = Field[from_x][from_y]
-                Field[from_x][from_y] = 0
-                Fields.append(copy.deepcopy(Field))
+            if do_move == "true":
+                self.Fields.append((from_x, from_y, self.Field[from_x][from_y], to_x, to_y, self.Field[to_x][to_y]))
+                self.Field[to_x][to_y] = self.Field[from_x][from_y]
+                self.Field[from_x][from_y] = 0
             return "ok"
-    if Field[from_x][from_y] in (3, -3, 5, -5):
-        if (abs(to_y - from_y) == abs(to_x - from_x) and
-                [Field[from_x + i * ((to_x - from_x) // abs(from_x - to_x))][from_y + i * ((to_y - from_y) // abs(from_y - to_y))]
-                 for i in range(1, abs(from_x - to_x))] == [0] * (abs(from_x - to_x) - 1)):
-            if move == "true":
-                Field[to_x][to_y] = Field[from_x][from_y]
-                Field[from_x][from_y] = 0
-                Fields.append(copy.deepcopy(Field))
-            return "ok"
-    if Field[from_x][from_y] in (4, -4, -5, 5):
-        if to_x == from_x:
-            if [Field[from_x][i] for i in range(min(from_y, to_y) + 1, max(from_y, to_y))] == [0] * (abs(from_y - to_y) - 1):
-                if move == "true":
-                    Field[to_x][to_y] = Field[from_x][from_y]
-                    Field[from_x][from_y] = 0
-                    Fields.append(copy.deepcopy(Field))
-                return "ok"
-        if to_y == from_y:
-            if [Field[i][from_y] for i in range(min(from_x, to_x) + 1, max(from_x, to_x))] == [0] * (abs(from_x - to_x) - 1):
-                if move == "true":
-                    Field[to_x][to_y] = Field[from_x][from_y]
-                    Field[from_x][from_y] = 0
-                    Fields.append(copy.deepcopy(Field))
-                return "ok"
-    if Field[from_x][from_y] in (-6, 6):
-        if abs(to_y - from_y)<=1 and abs(to_x - from_x)<=1:
-            if move == "true":
-                Field[to_x][to_y] = Field[from_x][from_y]
-                Field[from_x][from_y] = 0
-                Fields.append(copy.deepcopy(Field))
-            return "ok"
-    return "ne_ok"
 
-@app.route('/search/<from_x>/<from_y>')
-def search(from_x, from_y):
-    from_x, from_y = int(from_x), int(from_y)
-    ls=[]
-    for i in range(8):
-        for j in range(8):
-            if move(from_x, from_y, i, j, "false") == "ok":
-                ls.append(str(i+8*j))
-                if Field[from_x][from_y] in (6, -6):
-                    will_be_attacked(from_x, from_y, i, j, ls)
-    return " ".join(ls)
 
-@app.route('/reverse')
-def reverse():
-    global Field
-    global Fields
-    if len(Fields)>1:
-        Field = copy.deepcopy(Fields[-2])
-        Fields.pop()
+games = []
+
+
+@app.route('/crate_game/<normal_chess>')
+def crate_game(normal_chess):
+    if normal_chess == "true":
+        games.append(Game(True))
+    else:
+        games.append(Game(False))
+    return str(len(games) - 1)
+
+
+@app.route('/move/<id>/<from_x>/<from_y>/<to_x>/<to_y>/<do_move>')
+def move(id, from_x, from_y, to_x, to_y, do_move):
+    from_x, from_y, to_x, to_y = int(from_x), int(from_y), int(to_x), int(to_y)
+    id = int(id)
+    if (games[id].move and games[id].Field[from_x][from_y] < 0) or (not games[id].move and games[id].Field[from_x][from_y] > 0):
+        return "ne_ok"
+    if games[id].normal_chess:
+        s = request.args.get('select')
+        if games[id].Field[from_x][from_y] * games[id].Field[to_x][to_y] > 0:
+            return "ne_ok"
+        ans = games[id].move_funcs[games[id].Field[from_x][from_y]](from_x, from_y, to_x, to_y, do_move)
+        if ans == "ok" and do_move == "true":
+            games[id].move = not games[id].move
+        return ans
+    else:
+        if from_x == to_x and from_y == to_y:
+            return "ok"
+        ls = list(map(int,search(id,from_x, from_y).split(" ")))
+        for i in range(len(ls)):
+            games[id].Field[ls[i]%8][ls[i]//8] = games[id].Field[from_x][from_y]
+        games[id].move = not games[id].move
         return "ok"
 
-@app.route('/show')
-def show():
-    return Field
 
-def will_be_attacked(from_x, from_y, i, j, ls):
-    p = Field[i][j]
-    Field[i][j] = Field[from_x][from_y]
-    Field[from_x][from_y] = 0
+@app.route('/search/<id>/<from_x>/<from_y>')
+def search(id, from_x, from_y):
+    id = int(id)
+    from_x, from_y = int(from_x), int(from_y)
+    normal_chess = games[id].normal_chess
+    games[id].normal_chess = True
+    ls = []
+    for i in range(8):
+        for j in range(8):
+            if move(id, from_x, from_y, i, j, "false") == "ok":
+                #games[id].Field[i][j] = games[id].Field[from_x][from_y]
+                ls.append(str(i + 8 * j))
+                if games[id].Field[from_x][from_y] in (6, -6):
+                    will_be_attacked(id, from_x, from_y, i, j, ls)
+    games[id].normal_chess = normal_chess
+    return " ".join(ls)
+
+
+@app.route('/reverse/<id>')
+def reverse(id):
+    id = int(id)
+    global games
+    if len(games[id].Fields) > 0:
+        games[id].Field[games[id].Fields[-1][0]][games[id].Fields[-1][1]] = games[id].Fields[-1][2]
+        games[id].Field[games[id].Fields[-1][3]][games[id].Fields[-1][4]] = games[id].Fields[-1][5]
+        games[id].Fields.pop()
+        games[id].move = not games[id].move
+        return "ok"
+    return "ne_ok"
+
+
+@app.route('/show/<id>')
+def show(id):
+    id = int(id)
+    return games[id].Field
+
+
+def will_be_attacked(id, from_x, from_y, i, j, ls):
+    id = int(id)
+    p = games[id].Field[i][j]
+    games[id].Field[i][j] = games[id].Field[from_x][from_y]
+    games[id].Field[from_x][from_y] = 0
     for ii in range(8):
         for jj in range(8):
-            if Field[i][j] * Field[ii][jj] < 0:
-                if move(ii, jj, i, j, "false") == "ok":
+            if games[id].Field[i][j] * games[id].Field[ii][jj] < 0:
+                if move(id, ii, jj, i, j, "false") == "ok":
                     ls.pop()
-                    Field[from_x][from_y] = Field[i][j]
-                    Field[i][j] = p
+                    games[id].Field[from_x][from_y] = games[id].Field[i][j]
+                    games[id].Field[i][j] = p
                     return
-    Field[from_x][from_y] = Field[i][j]
-    Field[i][j] = p
+    games[id].Field[from_x][from_y] = games[id].Field[i][j]
+    games[id].Field[i][j] = p
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run("0.0.0.0", port=5000)
